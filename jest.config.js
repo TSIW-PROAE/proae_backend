815830111b546daff76f04df223b14e730404dc3
Thales Macêdo <thalesmacedo1@live.com>
2025-03-29 12:56:22 -0300

    added jest config file

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: 'src',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+      '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: '../coverage',
+    testEnvironment: 'node',
+    reporters: [
+      'default',
+      ['jest-junit', {
+        outputDirectory: './coverage/junit',
+        outputName: 'junit.xml',
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+      }],
+    ],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+  }; 
