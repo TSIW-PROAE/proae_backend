ed64907b19bbe70472229f11218e48480a23dbff
Lucas Lima <luc.tech3337@gmail.com>
2025-04-05 23:44:30 -0300

    feat: add auth guard middleware

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import type { Request } from 'express';
+
+interface User {
+  id: string;
+  sessionId: string;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(request: Request): string {
+    const user = request['user'] as User;
+    return `Hello World! Your UserId is ${user.id}`;
   }
 }
