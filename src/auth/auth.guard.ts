f3682d721b505edcbba145ba125bc07b75a42cf0
Lucas Lima <luc.tech3337@gmail.com>
2025-04-05 23:48:52 -0300

    feat: add auth guard middleware

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,43 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { verifyToken } from '@clerk/backend';
+import type { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Token ausente');
+    }
+
+    try {
+      console.log('Verificando token:', token);
+      const { sub: userId, sid: sessionId } = await verifyToken(token, {
+        secretKey: process.env.CLERK_SECRET_KEY,
+      });
+
+      request['user'] = {
+        id: userId,
+        sessionId,
+      };
+
+      return true;
+    } catch (e) {
+      console.log(e);
+      throw new UnauthorizedException('Token inválido ou expirado');
+    }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+
+    return type === 'Bearer' ? token : undefined;
+  }
+}
