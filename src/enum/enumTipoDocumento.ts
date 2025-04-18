f250c26d89bbd384b3fd3a5f5c94681d9b265f52
Lucas Lima <luc.tech3337@gmail.com>
2025-04-18 18:38:00 -0300

    chore: update enum label

--- src/enum/enumTipoDocumento.ts
+++ src/enum/enumTipoDocumento.ts
@@ -1,6 +1,6 @@
 export enum EnumTipoDocumento {
   CPF = 'Comprovante de situação cadastral do cpf',
-  HISTORICO_ESCOLAR = 'Certificado de conclusão ou Histórico escolar do ensino médio',
+  HISTORICO_ESCOLAR = 'Cert. de conclusão ou Hist. escolar do ensino médio',
   RG = 'Documento de Identidade',
   COMPROVANTE_MATRICULA = 'Comprovante de matrícula',
 }
