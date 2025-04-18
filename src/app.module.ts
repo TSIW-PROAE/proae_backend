3deec27ac85840405cc002427f3575edd3c68056
Lucas Lima <luc.tech3337@gmail.com>
2025-04-18 18:36:35 -0300

    chore: include student module & db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { StudentModule } from './student/student.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './db/db.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    StudentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/db/db.config.ts
@@ -0,0 +1,19 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export const typeOrmConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  synchronize: false,
+  logging: false,
+};
+
+export const AppDataSource = new DataSource(typeOrmConfig);
