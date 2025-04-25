da2465edce6f8b6a1799c8b113cd9eace7ceafec
marcos12144@hormail.com <marcos12144@hotmail.com>
2025-04-24 22:10:48 -0300

    feat: adicionar complementar de delete edital

--- src/edital/edital.module.ts
+++ src/edital/edital.module.ts
@@ -4,9 +4,10 @@ import { EditalController } from './edital.controller';
 import { EditalService } from './edital.service';
 import { Edital } from '../entities/edital/edital.entity';
 import { EtapaInscricao } from 'src/entities/etapaInscricao/etapaInscricao.entity';
+import { ResultadoEtapa } from 'src/entities/resultadoEtapa/resultadoEtapa.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Edital, EtapaInscricao])],
+    imports: [TypeOrmModule.forFeature([Edital, EtapaInscricao, ResultadoEtapa])],
     controllers: [EditalController],
     providers: [EditalService],
 })
