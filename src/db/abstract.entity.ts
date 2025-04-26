52eafd839a7103c875c03ce9513bf1d0cf1ff417
marcos12144@hormail.com <marcos12144@hotmail.com>
2025-04-26 13:21:19 -0300

    refactor: remover comentario abstract

--- src/db/abstract.entity.ts
+++ src/db/abstract.entity.ts
@@ -3,10 +3,4 @@ import { PrimaryGeneratedColumn } from "typeorm";
 export abstract class AbstractEntity<T> {
     @PrimaryGeneratedColumn()
     id: number;
-
-    // constructor(entity: Partial<T>) {
-    //     console.log('constructor', entity);
-    //     Object.assign(this, entity);
-    //     console.log('this abs', this);
-    // }
 }
