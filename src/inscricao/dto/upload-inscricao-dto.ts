b9c2b51634ac5735aa47eb01ff176f00d38ee2fc
Jessica <jessbarbosa2000@gmail.com>
2025-04-27 20:04:48 -0300

    fix: adjust files

--- src/inscricao/dto/upload-inscricao-dto.ts
+++
@@ -1,9 +0,0 @@
-export class UpdateInscricaoDto {
-    alunoId?: number;
-    editalId?: number;
-    dataInscricao?: Date;
-    statusInscricao?: string;
-    formularioId?: number;
-    documentos?: number[]; // Similar ao DTO de criação
-  }
-  
