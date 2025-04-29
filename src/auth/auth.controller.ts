612e6cd245b0e073b31f803cdac2d07d9b52f305
Msalaroli <salaroli1998@gmail.com>
2025-04-28 23:43:33 -0300

    feat:update clerk password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Patch, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
+import { AuthGuard } from './auth.guard';
+import { UpdatePasswordDto } from './dto/updatepassword.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +11,15 @@ export class AuthController {
   @Post('signup')
   async signup(@Body() body: SignupDto) {
     return this.authService.signup(body);
+  }
+
+  @UseGuards(AuthGuard)
+  @Patch('update-password')
+  async updatePassword(
+    @Req() req:any,
+    @Body() body: UpdatePasswordDto,
+  ) {
+    const clerkId = req.user.clerkId
+    return this.authService.updatePassword(clerkId, body.password);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -77,4 +77,26 @@ export class AuthService {
       throw new BadRequestException('Erro ao realizar o cadastro');
     }
   }
+
+  async updatePassword(clerkId: string, password: string) {
+    try{
+      const user = await this.clerk.users.getUser(clerkId);
+
+      if (!user) {
+        throw new BadRequestException('Usuário não encontrado');
+      }
+
+      await this.clerk.users.updateUser(clerkId, {
+        password,
+      });
+
+      return {
+        sucesso: true,
+        mensagem: 'Senha atualizada com sucesso',
+      };
+    } catch (e) {
+      console.error(e);
+      throw new BadRequestException('Erro ao atualizar a senha');
+    }
+  }
 }
